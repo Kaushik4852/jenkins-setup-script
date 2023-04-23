117a483581b69edb9cecabb2ffe8eb1f8c88a504
Kaushik Rathva <79138524+Kaushik4852@users.noreply.github.com>
2023-04-23 19:47:04 +0530

    yes to add-repo

--- CapStone_Project/ansible_install_master.sh
+++ CapStone_Project/ansible_install_master.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y
 sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository --yes ppa:ansible/ansible 
 sudo apt update -y
 sudo apt install ansible -y
-ssh-keygen
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
